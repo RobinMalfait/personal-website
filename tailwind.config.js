7b9fd96fd032d45688ac6daa8d5a72bb55aaf7e6
Jacob <jacobtlamb462@gmail.com>
2020-11-14 14:21:43 -0800

    Extend hover to transitionTimingFunction variant

--- tailwind.config.js
+++ tailwind.config.js
@@ -81,6 +81,7 @@ module.exports = {
     extend: {
       boxShadow: ['dark'],
       margin: ['hover'],
+      transitionTimingFunction: ['hover'],
       ringColor: ['hover'],
       ringOffsetColor: ['hover'],
     },
